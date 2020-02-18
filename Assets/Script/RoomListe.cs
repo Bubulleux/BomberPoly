10c9cbf3a5b6a98eb13a2f0d17975e9f938524e2
Bubulle <bubulle-lol@yahoo.fr>
2020-02-18 21:54:50 +0100

    RoomList 0.1

---
+++ Assets/Script/RoomListe.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+public class RoomListe : MonoBehaviour
+{
+    public GameObject panelRoom;
+    public Transform contente;
+    public PunManager pun;
+    void Start()
+    {
+        Initiaze();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Initiaze()
+    {
+        foreach(GameObject _go in GameObject.FindGameObjectsWithTag("RoomPanel"))
+        {
+            Destroy(_go);
+        }
+
+        foreach(RoomInfo _room in pun.rooms)
+        {
+            GameObject _go = Instantiate(panelRoom, contente);
+            _go.transform.Find("Name").GetComponent<Text>().text = _room.Name;
+            _go.transform.Find("Player").GetComponent<Text>().text = _room.PlayerCount + "/" + _room.MaxPlayers;
+
+        }
+    }
+}
